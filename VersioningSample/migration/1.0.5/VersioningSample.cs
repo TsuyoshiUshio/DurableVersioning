9d533a22eac647386dcda1dc378f07e4b7c09eb4
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-08-19 12:31:10 -0700

    Adding wait feature for testing behavior of the Durable Functions

--- VersioningSample/migration/1.0.5/VersioningSample.cs
+++ VersioningSample/migration/1.0.5/VersioningSample.cs
@@ -7,42 +7,42 @@ using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using System;
 
-namespace VersioningSample
+namespace VersioningSample_1_0_5
 {
     public static class VersioningSample
     {
-        [FunctionName("VersioningSample_1_0_6")]
+        [FunctionName("VersioningSample_1_0_5")]
         public static async Task<List<string>> RunOrchestrator(
             [OrchestrationTrigger] DurableOrchestrationContext context)
         {
             var outputs = new List<string>();
 
             // Replace "hello" with the name of your Durable Activity Function.
-            outputs.Add(await context.CallActivityAsync<string>("VersioningSample_Hello_1_0_6", "Tokyo"));
-            outputs.Add(await context.CallActivityAsync<string>("VersioningSample_Hello_1_0_6", "Seattle"));
-            outputs.Add(await context.CallActivityAsync<string>("VersioningSample_Hello_1_0_6", "London"));
+            outputs.Add(await context.CallActivityAsync<string>("VersioningSample_Hello_1_0_5", "Tokyo"));
+            outputs.Add(await context.CallActivityAsync<string>("VersioningSample_Hello_1_0_5", "Seattle"));
+            outputs.Add(await context.CallActivityAsync<string>("VersioningSample_Hello_1_0_5", "London"));
 
             // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
             return outputs;
         }
 
-        [FunctionName("VersioningSample_Hello_1_0_6")]
+        [FunctionName("VersioningSample_Hello_1_0_5")]
         public static async Task<string> SayHello([ActivityTrigger] string name, ILogger log)
         {
             log.LogInformation($"Saying hello to {name}.");
             log.LogInformation($"Waiting for the long runing job... for 3 min");
-            await Task.Delay(TimeSpan.FromSeconds(30));
+            await Task.Delay(TimeSpan.FromMinutes(3));
             return $"Hello {name}!";
         }
 
-        [FunctionName("VersioningSample_HttpStart_1_0_6")]
+        [FunctionName("VersioningSample_HttpStart_1_0_5")]
         public static async Task<HttpResponseMessage> HttpStart(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")]HttpRequestMessage req,
             [OrchestrationClient]DurableOrchestrationClient starter,
             ILogger log)
         {
             // Function input comes from the request content.
-            string instanceId = await starter.StartNewAsync("VersioningSample_1_0_6", null);
+            string instanceId = await starter.StartNewAsync("VersioningSample_1_0_5", null);
 
             log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
 
